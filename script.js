079afc2f7a7f33cd866e6737608ae818bbfb0116
TanLayHow <s10206171@connect.np.edu.sg>
2021-02-13 04:02:02 +0800

    Added charts using chart.js

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-let url = 'https://disease.sh/v3/covid-19/all';
-
-fetch(url)
+fetch('https://disease.sh/v3/covid-19/all')
   .then(response => response.json())
   .then(function (data) {
-    console.log(data.cases);
-    console.log(data.todayCases);
-    console.log(data.deaths);
-    console.log(data.todayDeaths);
-    console.log(data.recovered);
-    console.log(data.todayRecovered);
-    document.getElementById("totalCases").innerHTML = data.cases;
-    document.getElementById("totalDeaths").innerHTML = data.deaths;
-    document.getElementById("todayCases").innerHTML = data.todayCases;
-    document.getElementById("todayDeaths").innerHTML = data.todayDeaths;
-  });
+    document.getElementById("totalcases").innerHTML = data.cases;
+    document.getElementById("totaldeaths").innerHTML = data.deaths;
+
+    new Chart(document.getElementById("bar-chart1"), {
+      type: 'bar',
+      data: {
+        labels: ["Today confirmed cases","Today confirmed deaths"],
+        datasets: [
+          {
+            backgroundColor: ["#780000", "#A00000"],
+            data: [data.todayCases,data.todayDeaths],
+            maxBarThickness: 100,
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+        title: {
+          display: true,
+          text: 'Today confirmed cases vs Today confirmed deaths'
+        },
+      }
+    });
+});
+
+fetch('https://api.apify.com/v2/key-value-stores/yaPbKe9e5Et61bl7W/records/LATEST?disableRedirect=true')
+  .then(response => response.json())
+  .then(function (data) {
+    new Chart(document.getElementById("bar-chart2"), {
+      type: 'bar',
+      data: {
+        labels: ["SG Active Cases"," SG Total Deceased"],
+        datasets: [
+          {
+            backgroundColor: ["#780000", "#A00000"],
+            data: [data["activeCases"],data["deceased"]],
+            maxBarThickness: 100,
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+        title: {
+          display: true,
+          text: 'SG Active Cases and SG Total Deceased'
+        },
+      }
+    });
+});
